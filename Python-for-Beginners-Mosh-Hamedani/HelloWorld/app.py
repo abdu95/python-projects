a289d284ee6c48ae9e01a89b41a2d78738dd6087
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-19 19:32:59 +0500

    add comment

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -4,4 +4,7 @@ print(' ||||')
 # python executes line by line
 
 # expression - produces a value
+# when Python interpreter tries to execute this line,
+# it first will evaluate the expression
+# expression produces 10 asterisks, then they are printed
 print('*' * 10)
