5f2473ded035b474a3b11806958ea0925b37d537
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-24 20:49:38 +0500

    add input exercise

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,7 +1,3 @@
-# function will print the message on the terminal
-# and will wait for the user to enter a value
-# whatever the user enters, this input function will return
-# we save it to var
 name = input('What is your name? ')
-# concatenation
-print('Hi ' + name)
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
