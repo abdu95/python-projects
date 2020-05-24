e0cecd87027ea774914c1d7c5ee5e4f3915c74d1
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-24 17:52:50 +0500

    saving value to variable

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,1 +1,7 @@
+# when this 10 is about to be stored in the memory,
+# first it will get converted to its binary representation (1010)
+# then it will be stored in the computers memory
 price = 10
+price = 20
+# now price is 20, because python interprets line by line
+print(price)
