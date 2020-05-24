217bcbc853a6fc90689716d642d3386c8f0626d6
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-24 18:08:10 +0500

    exercise

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,13 +1,4 @@
-# integer
-price = 20
-# floating point number - float
-rating = 4.9
-# string
-name = 'Abdu'
-# boolean
-# '_' - used to separate multiple words in a variables name
-# Python is case sensitive PL.
-# when defining variables we should always use lowercase letters
-# False and True are special keywords in the language, so uppercase
-is_published = False
-print(price)
+age = 20
+name = 'John Smith'
+is_new_patient = True
+print(name)
