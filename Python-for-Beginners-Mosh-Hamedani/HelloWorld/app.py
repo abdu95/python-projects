37b79343495a607fba6c1d6298d0ab4b3463a445
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-27 02:20:56 +0500

    add quotation files

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,1 +1,14 @@
+# both single and double quotation is fine to use for strings
 course = 'Python for Beginners'
+# single quotation in the middle
+course = "Python's Course for Beginners"
+# double quotation in the middle
+course = 'Python for "Beginners"'
+# multiple lines >> triple quotes
+email = '''
+Hi John
+Here is our first email to you. 
+Thank you,
+The Support Team.
+'''
+print(email)
