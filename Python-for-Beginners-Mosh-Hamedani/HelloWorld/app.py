1166e0092e481ed1423fd3655189f30ac02d08f0
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-26 18:38:47 +0500

    add types

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,5 +1,11 @@
 # ask a DoB, calculate age, print on the terminal
 birth_year = input('Birth year: ')
-# Type error: we're subtracting a string from an integer
-age = 2020 - birth_year
+# Type error: we're subtracting a string (input) from an integer (2020)
+# age = str(2020) + birth_year --> possible
+# age = str(2020) - birth_year --> not possible
+age = 2020 - int(birth_year)
 print (age)
+
+# int() -- str to int
+# float() -- str to float
+# bool() -- str to boolean
