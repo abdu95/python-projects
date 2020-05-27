88802a3cd66e9779ad99d3b96e5c524768b9592e
abdumalik2014 <abdumalik2014@gmail.com>
2020-05-27 12:44:39 +0500

    add square bracket in string

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,14 +1,25 @@
-# both single and double quotation is fine to use for strings
 course = 'Python for Beginners'
-# single quotation in the middle
-course = "Python's Course for Beginners"
-# double quotation in the middle
-course = 'Python for "Beginners"'
-# multiple lines >> triple quotes
-email = '''
-Hi John
-Here is our first email to you. 
-Thank you,
-The Support Team.
-'''
-print(email)
+# [0] --> 'P'
+print(course[0])
+# negative index: get char starting from the end
+# second char from the end: 'r'
+print(course[-2])
+# Python interpreter will return all the characters
+# starting with this index [0] all the way to this second index [3]
+# but it does not return the character at this [3] index ('h').
+# --> Pyt
+print(course[0:3])
+# default values for start and end index
+# return all the characters till the end of the string --> 'Python for Beginners'
+print(course[0:])
+# exclude first character and return all the characters till the end of the string
+print(course[1:])
+# --> Pytho
+print(course[:5])
+# 0 will be assumed as start index, length of the string will be assumed as the end index
+print(course[:])
+# copy of course variable
+another = course[:]
+print('another ' + another)
+
+
