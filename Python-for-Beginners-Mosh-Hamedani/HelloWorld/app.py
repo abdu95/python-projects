ff854f3514bcf1aeddc89b2ee80690dec6640052
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-03 00:22:46 +0500

    add math

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,3 +1,15 @@
-name = input('What is your name? ')
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
+import math
+
+x = 2.9
+print(round(x))
+print(abs(-2.5))
+#  if you want to write a program that involves complex mathematical calculations,
+#  you need to import the math module
+#  A module in Python is a separate file with some reusable code.
+#  math is an object like a string, so we can access it's methods using the dot operator
+
+y = 2.4
+print(math.ceil(y))
+print(math.floor(y))
+# Google: Python 3 math module
+
