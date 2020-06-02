7707bd3646897911b85056e45c43d128941a1776
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-02 23:36:58 +0500

    add arithmetics

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,3 +1,13 @@
-name = input('What is your name? ')
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
+#numbers: integers - 10 & floats - numbers with decimal point
+# we get floating point number
+print(10/3)
+# we get integer
+print(10//3)
+# remainder
+print(10%3)
+# power
+print(10 ** 3)
+# augmented assignment operator
+x = 10
+x += 3
+print(x)
