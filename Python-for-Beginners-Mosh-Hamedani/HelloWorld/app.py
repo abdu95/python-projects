5eb1de51093327eb43e8d5ac4250585b411bd979
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-05 16:24:49 +0500

    add if exercise

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/app.py
@@ -1,6 +1,13 @@
 # house: 1M $
 # if the buyer has good credit, they will need to put down 10 percent of the price of this property
 # otherwise they need to put down 20 percent
+# display payment
 price = 1000000
+has_good_credit = True
 
-if
+if has_good_credit:
+    price = price - (price * 0.1)
+else:
+    price = price - (price * 0.2)
+
+print(f"Payment: {price}")
