7e1f13f616463994c41ad8addbb15a217a1dd1ec
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-05 20:46:27 +0500

    add logical operators

--- Python-for-Beginners-Mosh-Hamedani/HelloWorld/logical-op.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorld/logical-op.py
@@ -1,8 +1,27 @@
 #  If an applicant has high income and good credit,
 #  then they're eligible for a loan
 
-has_high_income = True
+# AND: both
+# OR: at least one
+# NOT:
+
+has_high_income = False
 has_good_credit = True
 
-if has_high_income and has_good_credit:
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible")
+
+if has_high_income or has_good_credit:
     print("Eligible for loan")
+else:
+    print("Not eligible")
+
+
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
