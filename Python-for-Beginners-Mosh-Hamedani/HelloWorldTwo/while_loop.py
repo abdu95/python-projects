50d1cafd65c9978409dc27d8c1f7a467f8d23a27
abdumalik2014 <abdumalik2014@gmail.com>
2020-06-09 23:46:45 +0500

    add guessing game exercise attempt 1

--- Python-for-Beginners-Mosh-Hamedani/HelloWorldTwo/while_loop.py
+++ Python-for-Beginners-Mosh-Hamedani/HelloWorldTwo/while_loop.py
@@ -1,6 +1,22 @@
-# i = 2 --> **
-i = 1
-while i <= 5:
-    print('*' * i)
-    i = i+1
-print("Done")
+import random
+random_number = random.randint(1,10)
+
+# if user guess is equal to random_number, say you win
+# check which attempt
+#    if attempt < 3
+#   user guess is not equal to random_number, increment i, ask input again
+i = 0
+number_found = False
+
+while i < 3 or number_found == False:
+    users_guess = int(input("Guess: "))
+    if users_guess == random_number:
+        print("You win")
+        number_found = True
+    else:
+        i = i + 1
+
+if number_found == False:
+    print("Sorry you failed")
+
+# 3 chances to make a guess
